e5637bd61a1797596bab6f5d80cdd0f0a91c1ea1
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2023-11-27 21:45:18 +0530

    Alpha ready

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,8 @@ import {fileExists, getCurrentProjectFolder, getFullPath} from "../src/shared/os
 import {stopSourceWatcher, watchFile} from "../src/sourcewatcher.js";
 import {startServer} from "../src/sliderunner.js";
 import {openApp} from "../src/browser/entrypoint.js";
+import path from "path";
+import fs from "fs";
 async function main(filename) {
 
     if(fileExists(filename)){
@@ -43,21 +45,60 @@ function cleanup() {
     process.exit(0);
 }
 
+function  getMarkdownFileFromCurrentDirectory(){
+    return new Promise((resolve, reject) => {
+        const directory = process.cwd(); // Gets the current working directory
+
+        fs.readdir(directory, (err, files) => {
+            if (err) {
+                reject('Error reading directory: ' + err);
+                return;
+            }
+
+            const markdownFiles = files.filter(file => path.extname(file).toLowerCase() === '.md');
+
+            if (markdownFiles.length === 0) {
+                reject('No Markdown files found');
+            } else if (markdownFiles.length > 1) {
+                reject('More than one Markdown file found');
+            } else {
+                resolve(path.join(directory, markdownFiles[0])); // Get the full path of the first file
+            }
+        });
+    });
+
+}
+
 (async () => {
 
 
     try {
+        let filename = null
+
         const params =process.argv.slice(2);
         if(params.length === 0){
-            console.log("Please provide a filename")
+            console.log(process.cwd())
+
+            filename = await getMarkdownFileFromCurrentDirectory()
+
+            console.log("Using file from current directory "+ filename)
+
+
+        }else{
+
+             filename = params[0];
+        }
+
+        if(filename === null){
+            console.log("No file provided")
             process.exit(1);
         }
-        const filename = params[0];
+
        await main(filename);
 
 
     }catch (err) {
-        console.log("Unable to Execute Command" , err)
+        console.log("Error" , err)
 
         process.exit(1);
     }
